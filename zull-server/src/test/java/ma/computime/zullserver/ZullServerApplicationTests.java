711cb6df629be614e2287d49e799d51fe2f98cc9
Ali M’hammedi Alaoui <48255868+alilou1998@users.noreply.github.com>
2020-08-20 16:26:27 +0100

    Add files via upload

---
+++ zull-server/src/test/java/ma/computime/zullserver/ZullServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.computime.zullserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
